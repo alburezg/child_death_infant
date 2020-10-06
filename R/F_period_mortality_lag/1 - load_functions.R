14e85e1892e7d01828e8784d22e8378122062c04
alburezg <gatemonte@gmail.com>
2020-10-06 12:09:17 +0200

    lag mort

---
+++ R/F_period_mortality_lag/1 - load_functions.R
@@ -0,0 +1,6 @@
+source("../functions.R")
+
+# Data wrangling
+wrangling <- c("tidyverse", "data.table","reshape2", "scales", "gridExtra", "directlabels", 'parallel', 'countrycode')
+
+library2(wrangling)
