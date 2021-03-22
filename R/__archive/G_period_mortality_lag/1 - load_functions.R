fd41cb4d785c6b594e77a9b33bce11c963b5f811
alburezg <gatemonte@gmail.com>
2021-03-22 08:55:25 +0100

    remove all _archive directories

--- R/__archive/G_period_mortality_lag/1 - load_functions.R
+++
@@ -1,6 +0,0 @@
-source("../functions.R")
-
-# Data wrangling
-wrangling <- c("tidyverse", "data.table","reshape2", "scales", "gridExtra", "directlabels", 'parallel', 'countrycode')
-
-library2(wrangling)
